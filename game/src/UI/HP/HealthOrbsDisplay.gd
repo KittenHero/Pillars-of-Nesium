3a81aa963701e6d608c25b3ba7425d9107d9d017
HotDogLegend <kevinlee78910@gmail.com>
2022-01-02 15:27:29 +1100

    adjusted health orb flashes

--- game/src/UI/HP/HealthOrbsDisplay.gd
+++ game/src/UI/HP/HealthOrbsDisplay.gd
@@ -13,11 +13,12 @@ func _on_health_updated(health, amount): #will re-draw the health orbs
 	for n in orbs.get_children():
 		n.queue_free()
 	
-	# Note inconsistency
+	# Note
 	# No exact reason to use n_orbs
 	# Introduce new mechanic when gaining health over max_health?
 	var n_orbs = max_health / 5 
 	var c_health = health
+	var is_flashed = false
 	for n in range(n_orbs):
 		#
 		var orb = hp_orb.instance()
@@ -30,19 +31,27 @@ func _on_health_updated(health, amount): #will re-draw the health orbs
 		elif c_health == 4:
 			c_health-=4
 			orb.play_anim("hp4")
+			is_flashed = true
 		elif c_health == 3:
 			c_health-=3
 			orb.play_anim("hp3")
+			is_flashed = true
 		elif c_health == 2:
 			c_health-=2
 			orb.play_anim("hp2")
+			is_flashed = true
 		elif c_health == 1:
 			c_health-=1
 			orb.play_anim("hp1")
+			is_flashed = true
+		elif !is_flashed:
+			orb.play_anim("emptywithflash")
+			is_flashed = true
 		else:
 			orb.play_anim("empty")
 			
 
 func set_max_health(value): #intented to start
 	max_health = value
+	_on_health_updated(max_health, 0)
 	
