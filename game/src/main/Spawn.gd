153a268249e7732552b78d56e2cd6dece1f5d5cf
NarisR <nariscatboy@gmail.com>
2022-01-04 15:11:02 +1100

    merge changes from Jacob

--- game/src/main/Spawn.gd
+++ game/src/main/Spawn.gd
@@ -1,15 +1,20 @@
-extends Node
+extends Node2D
 
 const MCPS = preload("res://src/Actors/MC.tscn")
 var mc
 
+func _ready():
+  Globals.set_spawnpoint(self)
+
+#func _ready():
+	#Globals.update_spawn(self.global_position)
 
 func spawn_player() -> MC:
 	if mc:
 		remove_child(mc)
 	mc = MCPS.instance()
 	add_child(mc)
-	mc.global_position = self.global_position
+#	mc.global_position = Globals.spawn_point
 	return mc
 
 func _on_dead() -> void:
