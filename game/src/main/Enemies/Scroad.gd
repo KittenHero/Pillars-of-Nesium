ca559d47562d1a20aa5ed7d7efdc35e4a47fdef1
Naris Rangsiyawaranon <nariscatboy@gmail.com>
2022-01-05 18:37:25 +1100

    fix scrode dialogue signals

--- game/src/main/Enemies/Scroad.gd
+++ game/src/main/Enemies/Scroad.gd
@@ -1,4 +1,8 @@
 extends KinematicBody2D
-class_name Scroad
-
 export var max_health = 25
+
+signal touching_player()
+
+func _on_Hitbox_area_entered(area):
+	if area != $Hurtbox:
+		emit_signal("touching_player")
